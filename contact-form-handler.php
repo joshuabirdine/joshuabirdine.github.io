d09721c2836b316442eb1412eee9f644f9f5569f
joshuabirdine <joshbird5@gmail.com>
2019-12-30 15:58:58 -0800

    Initial commit

---
+++ contact-form-handler.php
@@ -0,0 +1,25 @@
+<?php
+    $name = $_POST['name'];
+    $cell = $_POST['cell'];
+    $visitor_email = $_POST['email'];
+
+    $email_from = 'creambaja@gmail.com';
+
+    $email_subject = "New Form Submission";
+
+    $email_body = "Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                        "User Cell: $cell.\n";
+
+    $to = "joshbird5@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers = "Reply-To: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
+
+
+    ?>
